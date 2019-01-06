221b9a5ab641af600ef89e97ccb54b4c31813785
Yuta Koreeda <koreyou@mac.com>
2019-01-06 16:24:48 +0900

    Diferrent parameters for initial convolution for different heads

--- nets/gat.py
+++ nets/gat.py
@@ -79,11 +79,12 @@ class GraphAttentionConvolution(chainer.Chain):
         if out_size is None:
             in_size, out_size = None, in_size
         self.out_size = n_heads * out_size
+        self.n_heads = n_heads
 
         with self.init_scope():
             if initialW is None:
                 initialW = initializers.GlorotUniform()
-            self.W = chainer.Parameter(initialW, (in_size, out_size))
+            self.W = chainer.Parameter(initialW, (1, self.n_heads, in_size, out_size))
             if nobias:
                 self.b = None
             else:
@@ -91,12 +92,18 @@ class GraphAttentionConvolution(chainer.Chain):
                     initial_bias = 0
                 bias_initializer = initializers._get_initializer(initial_bias)
                 self.b = chainer.Parameter(bias_initializer, out_size)
-            self.attention_linear = L.Linear(out_size * 2, n_heads)
+            self.attention_W = chainer.Parameter(
+                initialW, (1, self.n_heads, out_size, 2))
 
     def attention(self, x, adj):
+        # x's shape: (nodes, n_heads, out_size)
         att = copy.deepcopy(adj)
-        h = F.hstack((x[adj.col], x[adj.row]))
-        h = self.attention_linear(h)
+        # e's shape: (nodes, n_heads, 2)
+        e = F.squeeze(F.matmul(x[:, :, None, :], self.attention_W), 2)
+        # e_row's shape: (nodes, n_heads, 1)
+        e_row, e_col = F.split_axis(e, 2, axis=2)
+        # Linear(concat(x_row, x_col)) = Linear_row(x_row) + Linear_col(x_col)
+        h = F.squeeze(e_row[adj.row] + e_col[adj.col], 2)
         att_data = F.leaky_relu(h, 0.2)
         # Scaling trick for numerical stability
         att_data -= self.xp.max(att_data.data)
@@ -104,7 +111,7 @@ class GraphAttentionConvolution(chainer.Chain):
         x = F.dropout(x, 0.6)
 
         output = []
-        for att_data_i in F.split_axis(att_data, att_data.shape[1], axis=1):
+        for att_data_i, xi in zip(F.split_axis(att_data, self.n_heads, axis=1), F.split_axis(x, self.n_heads, axis=1)):
             att.data = F.squeeze(att_data_i, 1)
             rowsum = F.sparse_matmul(
                 att, self.xp.ones([att.shape[1], 1], dtype=att.data.dtype))
@@ -113,19 +120,24 @@ class GraphAttentionConvolution(chainer.Chain):
             # but current sparse_matmul does not support two sparse matrix inputs
             att.data = att.data * rowsum[att.row]
             att.data = F.dropout(att.data, 0.6)
-            output.append(F.sparse_matmul(att, x))
+            output.append(F.sparse_matmul(att, F.squeeze(xi, 1)))
         output = F.concat(output, axis=1)
         return output
 
     def __call__(self, x, adj):
         if isinstance(x, chainer.utils.CooMatrix):
             x = copy.deepcopy(x)
-            x.data = F.dropout(x.data, 0.6)
-            x = F.sparse_matmul(x, self.W)
+            x_data = x.data
+            z = []
+            for i in range(self.n_heads):
+                x.data = F.dropout(x_data, 0.6)
+                z.append(F.sparse_matmul(x, self.W[0, i])[:, None, :])
+            z = F.concat(z, axis=1)
         else:
+            x = F.tile(x[:, None, :], (1, self.n_heads, 1))
             x = F.dropout(x, 0.6)
-            x = F.matmul(x, self.W)
-        output = self.attention(x, adj)
+            z = F.squeeze(F.matmul(x[:, :, None, :], self.W), 2)
+        output = self.attention(z, adj)
 
         if self.b is not None:
             output += self.b
